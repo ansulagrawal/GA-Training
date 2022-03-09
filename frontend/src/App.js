dfcd141154f55ca1a14f6bda124b045a3b93dcc0
Ansul Agrawal <ansulagrawal9@gmail.com>
2022-03-09 16:16:19 +0530

    Day 12 initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+   render() {
+      return <div className='App'></div>;
+   }
+}
+
+export default App;
+
+// Functional component
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
